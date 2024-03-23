3bef9d8437656faed6d248acaf0bc0aab5ddf30a
Kral67 <93152736+Kral6767@users.noreply.github.com>
2024-03-23 16:31:37 -0400

    user2 code

--- src/test/java/Proje1.java
+++ src/test/java/Proje1.java
@@ -1,5 +1,8 @@
 public class Proje1 {
     public static void main(String[] args) {
         System.out.println(" code 1");
+        //Adding second
+        System.out.println("user 2 code " );
     }
+
 }
