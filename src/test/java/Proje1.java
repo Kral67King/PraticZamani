f0fdab0c693d6290d183dc82abec33c38a5ffb76
Kral67 <93152736+Kral6767@users.noreply.github.com>
2024-05-19 13:39:59 -0400

    some changes too

--- src/test/java/Proje1.java
+++ src/test/java/Proje1.java
@@ -5,5 +5,6 @@ public class Proje1 {
         System.out.println("user 2 code " );
         System.out.println("user1 trying to add something");
         // 3  =4 is not correct
+        //not make sense
     }
 }
